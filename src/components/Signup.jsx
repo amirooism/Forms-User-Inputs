8fca52e46f9575d6efa8296faca6c76561e0e99b
amiro hatami <amirohatami@amiros-Laptop.local>
2024-11-06 10:28:31 +0330

    get all the inputs by usuing formData() build in the browser and to other method like entries() and getAll()

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,35 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault(); //Stop sending the automatically generated http request "We Put it to a onSubmit prop in the form element" :)
+    //FormData is a constructor function, build in browser, it is an object that makes it easy to get different values entered the form
+    //for using, we should pas the form as input to the FormData, and we can do it with pass event.target to it,
+    // because the target of this event we pass to the handleSubmit is the <form> :)
+    // all the input feilds or even select iput feilds must have the name attribute :) "To form data can give us access to the entered values"
+    // new FormData(event.target);
+    // put the result getting back from FormData to a const
+    const fd = new FormData(event.target);
+    //we can call with get method to exract the value for a spesefic name :)
+    // fd.get('email')
+    // const enteredEmail = fd.get("email");
+    // now we can repet that for all the input
+    // const enteredPassword = fd.get("password");
+    // but we end with a lot of code here for every input
+    //to getting all the enetr valuse in an object, use build in(Brwosers) objectClass and call fromEntries static method class and pass the formData object(fd) to it and call the entreis method to the object :
+    // Object.fromEntries(fd.entries())
+    //calling entreis give us an array fo the inputs and values, and calling the object.fromEntres, give us an object of these inputs
+    const data = Object.fromEntries(fd.entries());
+    // console.log(data);
+    // after consol log you see "How did find us group input" is not on the log
+    //they have the same name cause we want to find out what checked by the use, and they will lost when using entries method 
+    // we manually extracting and stroing them with getAll() method, we get multiple values from an input with the name of the checkbox elements and pass it to getAll
+    const acquisitionChannel = fd.getAll('acquisition')
+    // now we have to merg it to the data object "By adding new property to it" :
+data.acquisition = acquisitionChannel
+console.log(data);
+
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
